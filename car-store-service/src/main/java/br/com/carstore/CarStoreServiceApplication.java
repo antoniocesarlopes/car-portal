d6a2fcea1f46c0eb16c8524a1ba8ed873c598dc6
antoniocesar <alopes.java@gmail.com>
2023-02-24 19:26:14 -0300

    inicio projeto car post

---
+++ car-store-service/src/main/java/br/com/carstore/CarStoreServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.carstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarStoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarStoreServiceApplication.class, args);
+	}
+
+}
---
+++ car-store-service/src/test/java/br/com/alopes/CarStoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alopes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarStoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
